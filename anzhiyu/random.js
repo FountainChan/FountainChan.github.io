f6c68cedf8b1dfcd6f5464bae60ade374f8dd08e
FountainChan <FountainChan@users.noreply.github.com>
2025-09-30 09:46:28 +0000

    Deploy via GitHub Actions: 4afb79f91ad063e443fd547a837b06ad5946431f FountainChan/SourceHexoBlog@4afb79f91ad063e443fd547a837b06ad5946431f

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/29/hello-world/","2025/09/29/比较便宜的VPS推广之CloudCone/","2025/09/27/没有服务器，如何用github创建自己的Hexo的博客/"];function toRandomPost(){
+var posts=["2025/09/25/hello-world/","2025/09/29/比较便宜的VPS推广之CloudCone/","2025/09/26/没有服务器，如何用github创建自己的Hexo的博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
