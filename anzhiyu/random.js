e9631cfe78ae4a5732b388735c21d913b48661ac
FountainChan <FountainChan@users.noreply.github.com>
2026-07-16 08:02:00 +0000

    Deploy via GitHub Actions: 85a8e708ce76f17fb190b55ec24d1fd1cf224a2d FountainChan/SourceHexoBlog@85a8e708ce76f17fb190b55ec24d1fd1cf224a2d

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["tech/49746.html","tech/58871.html","tech/40447.html","tech/28493.html","life/42815.html","recommends/98de12e.html","tech/1ef6d324.html","life/60749.html","tech/57002.html","tech/56639.html","tech/28915.html"];function toRandomPost(){
+var posts=["tech/49746.html","tech/58871.html","tech/40447.html","tech/28493.html","life/42815.html","recommends/98de12e.html","tech/1ef6d324.html","life/60749.html","tech/55151.html","tech/57002.html","tech/56639.html","tech/28915.html"];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
